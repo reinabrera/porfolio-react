e18c10087108edea424739215cace47bfcc6691d
Rein <rein@offshorly.com>
2024-03-06 23:54:17 +0800

    skills: text-align center

--- src/components/Skills/SkillsItem.jsx
+++ src/components/Skills/SkillsItem.jsx
@@ -1,6 +1,6 @@
 export default function SkillsItems({data}) {
   return(<div className="item flex">
-    <div className="item-content flex items-center flex flex-center">
+    <div className="item-content flex items-center flex flex-center text-center">
       <img src={data.img} alt={data.name + " " +"icon"} />
       <span className="small">{data.name}</span>
     </div>
